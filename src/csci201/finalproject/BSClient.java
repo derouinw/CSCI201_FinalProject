0f56430bdc39185cb97b28d2be195f9a7bda97da
Bill Derouin <derouinbill@gmail.com>
2014-11-09 14:59:14 -0800

    Undo conflict tests

--- src/csci201/finalproject/BSClient.java
+++ src/csci201/finalproject/BSClient.java
@@ -4,14 +4,13 @@ public class BSClient {
 
 	class NetworkThread extends Thread {
 		public void run() {
-			// yolo 4
-			// yolo 5
+			
 		}
 	}
 	
 	class RunServerThread extends Thread {
 		public void run() {
-			// yolo 2
+			
 		}
 	}
 }

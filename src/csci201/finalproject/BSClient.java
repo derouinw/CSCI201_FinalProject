80ce9ae6f1cb95ed2ee78b749760d1a7892e344a
Bill Derouin <derouinbill@gmail.com>
2014-11-09 14:42:41 -0800

    Conflict test 2

--- src/csci201/finalproject/BSClient.java
+++ src/csci201/finalproject/BSClient.java
@@ -4,13 +4,13 @@ public class BSClient {
 
 	class NetworkThread extends Thread {
 		public void run() {
-		// yolo	1
+			
 		}
 	}
 	
 	class RunServerThread extends Thread {
 		public void run() {
-			
+			// yolo 2
 		}
 	}
 }

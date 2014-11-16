003babdec680fb1a5008c8eca0adaf2c3e48f5f9
Bill Derouin <derouinbill@gmail.com>
2014-11-15 19:32:33 -0800

    Implemented networking backbone in server and client

--- src/csci201/finalproject/ClientGUI.java
+++ src/csci201/finalproject/ClientGUI.java
@@ -3,7 +3,11 @@ package csci201.finalproject;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import csci201.finalproject.BSClient.NetworkThread;
+
 public class ClientGUI extends JFrame {
+	
+	public ClientGUI(NetworkThread nt) {}
 
 	class SplashGUI extends JPanel {
 		

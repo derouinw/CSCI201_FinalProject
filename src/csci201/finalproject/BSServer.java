9c0bbbb36b5d39a40641ec9e53da1d3fc700681a
Bill Derouin <derouinbill@gmail.com>
2014-11-23 13:48:42 -0800

    Server progress

--- src/csci201/finalproject/BSServer.java
+++ src/csci201/finalproject/BSServer.java
@@ -11,36 +11,36 @@ import java.util.ArrayList;
 public class BSServer {
 	// Port doesn't change
 	final static int PORT = 2000;
-	
+
 	// There should only be one ServerThread in a server
 	ServerThread st;
-	
+
 	// Store the number of players here too
 	int numPlayers;
-	
+
 	// Constructor
 	public BSServer(int numPlayers) {
 		this.numPlayers = numPlayers;
-		
+
 		try {
 			// ServerSocket accepts connections over internet
 			ServerSocket ss = new ServerSocket(PORT);
 			st = new ServerThread(numPlayers);
-			
+
 			int connections = 0;
 			while (connections < numPlayers) {
 				// once the first player has connected (the host)
 				// start the ServerThread
 				if (connections == 1)
 					st.start();
-				
+
 				// accept the connection from the ServerSocket
 				Socket s = ss.accept();
 				st.addPlayer(s);
-				
+
 				connections++;
 			}
-			
+
 			// this point is reached once all players have connected
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -53,16 +53,27 @@ public class BSServer {
 	class ServerThread extends Thread {
 		// Store a PlayerThread for each player's connection
 		ArrayList<PlayerThread> playerThreads;
-		
+
 		// Max number of players allowed to connect
 		int numPlayers;
+
+		// Corresponds to game state in ClientGUI
+		// options: “lobby” “fleet selection” “playing” “game over”
+		String gameState;
 		
+		// during fleet selection, how many players are ready
+		int fleetsFinished = 0;
+		
+		// during playing, the index of current player firing
+		int curPlayer = 0; // default host
+
 		// Constructor
 		public ServerThread(int numPlayers) {
 			this.numPlayers = numPlayers;
 			playerThreads = new ArrayList<PlayerThread>();
+			gameState = "lobby";
 		}
-		
+
 		// Creates a connection with socket passed in,
 		// creating a PlayerThread to handle it
 		public void addPlayer(Socket s) {
@@ -70,34 +81,84 @@ public class BSServer {
 			playerThreads.add(pt);
 			pt.start();
 		}
-		
+
 		// Receive a message from a PlayerThread
 		// (send from within PlayerThread.receive)
 		public void receive(String msg, String src) {
 			// take message from player and do what needs to be done
+			if (gameState.equals("lobby")) {
+				// messages during lobby
+			} else if (gameState.equals("fleet selection")) {
+				// if player hits ready
+				if (msg.trim().equals("ready")) {
+					fleetsFinished++;
+				} else if (msg.trim().equals("unready")) {
+					fleetsFinished--;
+				}
+			} else if (gameState.equals("playing")) {
+				// TODO: receive a message while playing
+			} else if (gameState.equals("game over")) {
+				// there should be no messages received during game over
+			}
 		}
-		
+
 		// Thread.run
 		// I.e. main loop for thread
 		public void run() {
-			// server logic here
+			while (true) { // TODO: real exit case
+				// server logic here
+				if (gameState.equals("lobby")) {
+					// send out message with player names
+					String msg = "users ";
+					for (int i = 0; i < playerThreads.size(); i++) {
+						msg += playerThreads.get(i).username + " ";
+					}
+					broadcast(msg);
+				} else if (gameState.equals("fleet selection")) {
+					// wait until all players are ready
+					if (fleetsFinished == numPlayers) {
+						gameState = "playing";
+						broadcast("ready");
+					}
+				} else if (gameState.equals("playing")) {
+					// TODO: playing part of run()
+				} else if (gameState.equals("game over")) {
+					// send out statstics data then kill server
+					// TODO: send statistics data
+					break;
+				}
+			}
+		}
+
+		// sends out a message to all players
+		private void broadcast(String msg) {
+			for (int i = 0; i < playerThreads.size(); i++) {
+				playerThreads.get(i).send(msg);
+			}
+		}
+		
+		private int ptNum(String name) {
+			return 0;
 		}
 	}
-	
+
 	// PlayerThread is the connection to a single player
 	class PlayerThread extends Thread {
 		Socket s;
-		
+
 		// To communicate with player
 		BufferedReader receive;
 		PrintWriter send;
-		
+
 		// To send messages received from player to server
 		ServerThread st;
-		
+
 		// Whether the player has been killed or not
 		boolean active;
-		
+
+		// the user's handle
+		String username;
+
 		// Constructor
 		public PlayerThread(Socket s, ServerThread st) {
 			this.s = s;
@@ -110,21 +171,28 @@ public class BSServer {
 				e.printStackTrace();
 			}
 		}
-		
+
 		// Send a message to the player
 		public void send(String s) {
 			send.println(s);
 		}
-		
+
 		// Thread.run
 		public void run() {
 			while (s.isConnected()) {
 				try {
 					String input = receive.readLine();
-					
+
 					// logic with received message
-					// e.g. 
-					// if (!input.contains("waiting")) st.receive(s)
+
+					// set username
+					if (input.startsWith("connect")) {
+						username = input.substring(input.trim().indexOf(" "))
+								.trim();
+						// otherwise everything is handled in the ServerThread
+					} else if (!input.startsWith("waiting")) {
+						st.receive(input, username);
+					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}

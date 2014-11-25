f72890a039cf78eb07331bc0f624281f840f1664
Max <mpweiner@usc.edu>
2014-11-25 15:06:51 -0800

    Ship Placement Done

--- src/csci201/finalproject/Ship.java
+++ src/csci201/finalproject/Ship.java
@@ -3,6 +3,8 @@ package csci201.finalproject;
 public abstract class Ship {
 
 	private int healthPoints;
+	boolean vertical;
+	Coordinate coord;
 	
 	public Ship() {
 		// TODO Auto-generated constructor stub
@@ -11,6 +13,32 @@ public abstract class Ship {
 	public void hit(){
 		if(healthPoints!=0){
 			healthPoints--;
+		}
+	}
+	
+	public class Dinghy extends Ship {
+		public Dinghy() {
+			healthPoints = 2;
+		}
+	}
+	public class Sloop extends Ship {
+		public Sloop() {
+			healthPoints = 3;
+		}
+	}
+	public class Frigate extends Ship {
+		public Frigate() {
+			healthPoints = 3;
+		}
+	}
+	public class Brigantine extends Ship {
+		public Brigantine() {
+			healthPoints = 4;
+		}
+	}
+	public class Galleon extends Ship {
+		public Galleon() {
+			healthPoints = 5;
 		}
 	}
 

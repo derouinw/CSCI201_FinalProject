eb8e63aa9056e13ff90e5906f55d6c42f6773254
Bill Derouin <derouinbill@gmail.com>
2014-11-30 13:48:23 -0800

    Handle disconnects better

--- src/csci201/finalproject/ClientGUI.java
+++ src/csci201/finalproject/ClientGUI.java
@@ -110,7 +110,9 @@ public class ClientGUI extends JFrame {
 		if (msg.value == null) {
 			// disconnect
 			JDialog popup = new JDialog(this, "Disconnected from server");
+			setVisible(false);
 			dispose();
+			nt.connected = false;
 		}
 		switch (msg.type) {
 		case Message.TYPE_STRING:

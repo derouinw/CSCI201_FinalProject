c84addc1b7eb4a3457519bb9a6530e306907db98
Cara Kirshon <kirshon@usc.edu>
2014-11-30 11:03:08 -0800

    Adding some end game gui

--- src/csci201/finalproject/Main.java
+++ src/csci201/finalproject/Main.java
@@ -5,21 +5,15 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 
 public class Main extends JFrame {
-	SplashGUI splashScreen;
-	LobbyGUI lobby;
-	FleetGUI fleet;
+	GameOverGUI go;
 	public Main() {
 		super("Buccaneer Battles");
 		setSize(800,800);
-		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
-		//splashScreen = new SplashGUI(this);
-		//lobby = new LobbyGUI();
-		//fleet = new FleetGUI();
-		add(fleet, BorderLayout.CENTER);
-		setSize(fleet.getPreferredSize());
-		setSize(800,600);
+		
+		go = new GameOverGUI();
+		add(go, BorderLayout.CENTER);
 	}
 	
 	public static void main(String[] args) {
